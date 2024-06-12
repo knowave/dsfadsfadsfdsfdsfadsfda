5b0e604e931ce643b9c497a611cbee6a14974b7e
knowave <kwaktaemin98@gmail.com>
2024-06-12 10:42:29 +0900

    Install styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import styled from "styled-components";
+import Feed from "./Feed";
+import { useMediaQuery } from "react-responsive";
+
+const AppContainer = styled.div`
+  text-align: center;
+`;
+
+const Header = styled.header`
+  background-color: #282c34;
+  min-height: 10vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+const MainContent = styled.main`
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const MobileMessage = styled.div`
+  background-color: #61dafb;
+  padding: 20px;
+  border-radius: 8px;
+  margin: 20px 0;
+  color: white;
+`;
 
 function App() {
+  const isDesktopOrLaptop = useMediaQuery({
+    query: "(min-width: 1224px)",
+  });
+  const isTabletOrMobile = useMediaQuery({
+    query: "(max-width: 1224px)",
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
+    <AppContainer>
+      <Header>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -18,8 +57,16 @@ function App() {
         >
           Learn React
         </a>
-      </header>
-    </div>
+      </Header>
+      <MainContent>
+        {isDesktopOrLaptop && <Feed />}
+        {isTabletOrMobile && (
+          <MobileMessage>
+            Welcome to the mobile version of our site!
+          </MobileMessage>
+        )}
+      </MainContent>
+    </AppContainer>
   );
 }
 
