4ec9cb3eed0de4ddfd7b835a7de0a64f39e09f7a
knowave <kwaktaemin98@gmail.com>
2024-06-21 17:41:42 +0900

    App.css import 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "./App.css";
 import Feed from "./feed/Feed";
 import { useMediaQuery } from "react-responsive";
 import { AppContainer, Header, MainContent } from "./css/app.css";
@@ -12,21 +11,11 @@ function App() {
   return (
     <AppContainer>
       <Header>
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
-          {/* Edit <code>src/App.tsx</code> and save to reload. */}
           {isDesktopOrLaptop
             ? "Welcome to the desktop version of the app!"
             : "You are on the mobile version of the app!"}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </Header>
       <MainContent>
         <Feed />
