17e3ffaa08a2dd49adf14e1e9bdfc022e291c10c
knowave <kwaktaemin98@gmail.com>
2024-06-21 17:40:04 +0900

    공개 피드 mock 데이터

---
+++ src/feed/mock/feed-items.mock.ts
@@ -0,0 +1,68 @@
+export const mockFeedItems = [
+  {
+    id: "uuid1",
+    thumbnail: "https://picsum.photos/200/300",
+    title: "Mock Title1",
+    content: "Mock Content1",
+  },
+  {
+    id: "uuid2",
+    thumbnail: "https://picsum.photos/200/300",
+    title: "Mock Title2",
+    content: "Mock Content2",
+  },
+  {
+    id: "uuid3",
+    thumbnail: "https://picsum.photos/200/300",
+    title: "Mock Title3",
+    content: "Mock Content3",
+  },
+  {
+    id: "uuid3",
+    thumbnail: "https://picsum.photos/200/300",
+    title: "Mock Title3",
+    content: "Mock Content3",
+  },
+  {
+    id: "uuid4",
+    thumbnail: "https://picsum.photos/200/300",
+    title: "Mock Title4",
+    content: "Mock Content4",
+  },
+  {
+    id: "uuid5",
+    thumbnail: "https://picsum.photos/200/300",
+    title: "Mock Title5",
+    content: "Mock Content5",
+  },
+  {
+    id: "uuid6",
+    thumbnail: "https://picsum.photos/200/300",
+    title: "Mock Title6",
+    content: "Mock Content6",
+  },
+  {
+    id: "uuid7",
+    thumbnail: "https://picsum.photos/200/300",
+    title: "Mock Title7",
+    content: "Mock Content7",
+  },
+  {
+    id: "uuid8",
+    thumbnail: "https://picsum.photos/200/300",
+    title: "Mock Title8",
+    content: "Mock Content8",
+  },
+  {
+    id: "uuid9",
+    thumbnail: "https://picsum.photos/200/300",
+    title: "Mock Title9",
+    content: "Mock Content9",
+  },
+  {
+    id: "uuid10",
+    thumbnail: "https://picsum.photos/200/300",
+    title: "Mock Title10",
+    content: "Mock Content10",
+  },
+];
